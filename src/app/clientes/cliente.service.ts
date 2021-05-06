d9b0ecff337546c14b042a63599ba1d3a51bac0e
Pablo Lopez Burgos <pablo.lopez.external@atos.net>
2021-05-06 18:53:27 +0200

    copy

---
+++ src/app/clientes/cliente.service.ts
@@ -0,0 +1,118 @@
+import { Injectable } from '@angular/core';
+import {formatDate, DatePipe} from '@angular/common'
+/*import { CLIENTES } from './clientes.json';*/
+import { Cliente } from './cliente';
+import { of, Observable, throwError} from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import swal from 'sweetalert2'
+import { Router } from '@angular/router';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+  private urlEndPoint:string = 'http://localhost:8080/api/clientes'
+
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getClientes(page: number): Observable<any> {
+    return this.http.get(this.urlEndPoint + '/page/' + page).pipe(
+      tap( (response: any) => {
+        console.log('ClienteService: tap 1');
+        (response.content as Cliente[]).forEach( cliente => {
+          console.log(cliente.nombre);
+        })
+      }),
+      map( (response: any) => {
+        (response.content as Cliente[]).map(cliente => {
+          cliente.nombre = cliente.nombre.toUpperCase();
+          //let datePipe = new DatePipe('es');
+          //cliente.createAt = datePipe.transform(cliente.createAt, 'EEEE dd, MMMM yyyy');
+          //formatDate(cliente.createAt, 'dd-MM-yyyy', 'en-US')
+          return cliente;
+        });
+        return response;
+      }
+    ),
+      tap( (response: any) => {
+        console.log('ClienteService: tap 2');
+        (response.content as Cliente[]).forEach( cliente => {
+          console.log(cliente.nombre);
+        })
+      })
+      );
+  }
+
+  getCliente(id): Observable<Cliente> {
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+      catchError( e => {
+        this.router.navigate(['/clientes']);
+        console.error(e.error.mensaje);
+        swal.fire('Error al editar', e.error.error, 'error');
+        return throwError(e);
+      })
+    );
+  }
+
+  create(cliente: Cliente) : Observable<Cliente> {
+    return this.http.post(this.urlEndPoint, cliente, {headers: this.httpHeaders}).pipe(
+      map((response: any) => response.cliente as Cliente),
+      catchError(e => {
+
+        if (e.status==400){
+          return throwError(e);
+        }
+
+        console.error(e.error.mensaje);
+        swal.fire(e.error.mensaje, e.error.error, 'error');
+        return throwError(e);
+      })
+    );
+  }
+
+  update(cliente: Cliente): Observable<any>{
+    return this.http.put<any>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+
+        if (e.status==400){
+          return throwError(e);
+        }
+
+        console.error(e.error.mensaje);
+        swal.fire(e.error.mensaje, e.error.error, 'error');
+        return throwError(e);
+      })
+    );
+  }
+
+  delete(id: number): Observable<Cliente>{
+    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+        console.error(e.error.mensaje);
+        swal.fire(e.error.mensaje, e.error.mensaje, 'error');
+        return throwError(e);
+      })
+    );
+  }
+
+  subirFoto(archivo: File, id): Observable<Cliente> {
+    let formData = new FormData();
+    formData.append("archivo", archivo);
+    formData.append("id", id);
+
+    return this.http.post(`${this.urlEndPoint}/upload/`, formData).pipe(
+      map((response: any) => response.cliente as Cliente),
+      catchError(e => {
+        console.error(e.error.mensaje);
+        swal.fire(e.error.mensaje, e.error.mensaje, 'error');
+        return throwError(e);
+      })
+    );
+
+  }
+
+}
---
+++ src/app/clientes/form.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import {Cliente} from './cliente'
+import {ClienteService} from './cliente.service'
+import {Router, ActivatedRoute} from '@angular/router'
+import swal from 'sweetalert2'
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+})
+export class FormComponent implements OnInit {
+
+  cliente: Cliente = new Cliente()
+  titulo:String = "Crear cliente"
+  errores: string[];
+
+  constructor(private clienteService: ClienteService,
+  private router: Router,
+  private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.cargarCliente()
+  }
+
+  cargarCliente(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id']
+      if (id){
+        this.clienteService.getCliente(id).subscribe( (cliente) => this.cliente = cliente)
+      }
+    })
+  }
+
+  public create(): void {
+    this.clienteService.create(this.cliente)
+    .subscribe(cliente => {
+        this.router.navigate(['/clientes'])
+        swal.fire('Nuevo cliente', `El cliente ${cliente.nombre} ha sido creado con exito`, 'success' )
+      },
+      err => {
+        this.errores = err.error.errors as string[];
+        console.error("Codigo del error desde el backend: " + err.status);
+        console.error(err.error.errors);
+
+      }
+    );
+  }
+
+  update(): void {
+    this.clienteService.update(this.cliente)
+    .subscribe( json => {
+      this.router.navigate(['/clientes'])
+      swal.fire('Cliente Actualizado', `${json.mensaje}: ${json.cliente.nombre}`, 'success')
+    },
+    err => {
+      this.errores = err.error.errors as string[];
+      console.error("Codigo del error desde el backend: " + err.status);
+      console.error(err.error.errors);
+
+    }
+  )
+  }
+
+}
